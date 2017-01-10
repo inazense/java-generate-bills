83e8e4e8702c5276e4f1d262262258a193dc76f5
Inazio Claver <jclaver64@gmail.com>
2017-01-11 00:35:54 +0100

    Second version of FrameSearchBIlls

--- src/gui/FrameSearchBills.java
+++ src/gui/FrameSearchBills.java
@@ -1,6 +1,9 @@
 package gui;
 
 import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
@@ -15,7 +18,19 @@ public class FrameSearchBills extends MyFrame {
 	
 	private JFormattedTextField txtDate;
 	
+	private JLabel lblTitle;
+	private JLabel lblBill;
+	private JLabel lblDate;
+	private JLabel lblClient;
+	private JLabel lblHelp;
+	
+	private JScrollPane scrollClient;
+	private JScrollPane scrollBill;
+	
+	private JSeparator separator;
+	
 	private JTable tblBills;
+	
 	private JTextField txtBillNumber;
 	
 	// Constructor
